528a65bdeba39c0c35f2c682a9396b7b00d438af
Carlos Marinho <carluizfla@hotmail.com>
2022-08-22 20:19:47 -0300

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(email: 'jomar@email.com', password: '123456', name: 'Jomar', username: "jomar", birthday: "05/05/1990")
-User.create!(email: 'carlos@hotmail.com', password: '123456', name: 'Carlos Luiz', username: "carlosm", birthday: "24/02/1990")
+User.create!(email: 'jomar@email.com', password: '123456', name: 'Jomar', username: "jomar", birthday: "05/05/1990", superadmin_role: true)
+User.create!(email: 'carlos@hotmail.com', password: '123456', name: 'Carlos Luiz', username: "carlosm", birthday: "24/02/1990", superadmin_role: true)
+User.create!(email: 'eloah@gmail.com', password: '123456', name: 'Eloah Silva', username: "carlosm", birthday: "17/10/1986")
+User.create!(email: 'cadu@gmail.com', password: '123456', name: 'Cadu Silva', username: "cadu", birthday: "18/12/2000")
+User.create!(email: 'juca@gmail.com', password: '123456', name: 'Carlos Silva Pai', username: "juca", birthday: "18/02/1960")
+User.create!(email: 'marli@gmail.com', password: '123456', name: 'Marli França', username: "marli", birthday: "12/12/1960")
+User.create!(email: 'sueli@gmail.com', password: '123456', name: 'Sueli da Costa Silva', username: "sueli", birthday: "10/04/1956")
