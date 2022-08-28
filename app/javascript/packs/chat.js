be36dac017a1b47b246701cacb9f5d776e3b7dc3
Carlos Marinho <carluizfla@hotmail.com>
2022-08-28 16:41:37 -0300

    correct the chat.js file and add an image

--- app/javascript/packs/chat.js
+++ app/javascript/packs/chat.js
@@ -1,12 +1,24 @@
 var btn = document.querySelector('#btn-square');
+var btn2 = document.querySelector('#btn-more');
 var container = document.querySelector('.list-image-square');
+var container2 = document.querySelector('.list-image-more');
 
 btn.addEventListener('click', function () {
-  if (container.style.display === 'block') {
-    container.style.display = 'none';
-  } else {
-    container.style.display = 'block';
-  }
+    if (container.style.display === 'block') {
+        container.style.display = 'none';
+    } else {
+        container.style.display = 'block';
+    }
 
-  console.log('o que', container);
+    console.log('o que', container);
+});
+
+btn2.addEventListener('click', function () {
+    if (container2.style.display === 'block') {
+        container2.style.display = 'none';
+    } else {
+        container2.style.display = 'block';
+    }
+
+    console.log('o que', container2);
 });
