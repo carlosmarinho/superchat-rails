3d17b5d105300ff552e8eceab2fd162b37c2aacb
Carlos Marinho <carluizfla@hotmail.com>
2022-08-30 19:54:52 -0300

    refator: change var to const

--- app/javascript/packs/chat.js
+++ app/javascript/packs/chat.js
@@ -1,47 +1,47 @@
-var btn = document.querySelector('#btn-square');
-var container = document.querySelector('.list-image-square');
+const btn = document.querySelector('#btn-square');
+const container = document.querySelector('.list-image-square');
 
-var btn2 = document.querySelector('#btn-more');
-var container2 = document.querySelector('.list-image-more');
+const btn2 = document.querySelector('#btn-more');
+const container2 = document.querySelector('.list-image-more');
 
-var conversations = document.querySelectorAll('.conversation');
-var chat = document.querySelector('.content-chat');
+const conversations = document.querySelectorAll('.conversation');
+const chat = document.querySelector('.content-chat');
 
-var back = document.querySelector('.header-icons-2');
+const back = document.querySelector('.header-icons-2');
 
 btn.addEventListener('click', function () {
-    if (container.style.display === 'block') {
-        container.style.display = 'none';
-    } else {
-        container.style.display = 'block';
-    }
+  if (container.style.display === 'block') {
+    container.style.display = 'none';
+  } else {
+    container.style.display = 'block';
+  }
 });
 
 btn2.addEventListener('click', function () {
-    if (container2.style.display === 'block') {
-        container2.style.display = 'none';
-    } else {
-        container2.style.display = 'block';
-    }
+  if (container2.style.display === 'block') {
+    container2.style.display = 'none';
+  } else {
+    container2.style.display = 'block';
+  }
 });
 
 conversations.forEach((conversation) => {
-    conversation.addEventListener('click', function () {
-        // comentei aqui tenho q ver como vai ficar com essa seta ou não
-        // if (chat.style.display === 'block') {
-        //   chat.style.display = 'none';
-        // } else {
-        //   chat.style.display = 'block';
-        // }
+  conversation.addEventListener('click', function () {
+    // comentei aqui tenho q ver como vai ficar com essa seta ou não
+    // if (chat.style.display === 'block') {
+    //   chat.style.display = 'none';
+    // } else {
+    //   chat.style.display = 'block';
+    // }
 
-        window.location = `/?id=${conversation.dataset.conversationId}`;
-    });
+    window.location = `/?id=${conversation.dataset.conversationId}`;
+  });
 });
 
 back.addEventListener('click', function () {
-    if (chat.style.display === 'none') {
-        chat.style.display = 'block';
-    }
+  if (chat.style.display === 'none') {
+    chat.style.display = 'block';
+  }
 });
 
 const backConversation = document.getElementById('back-to-conversation');
@@ -60,6 +60,6 @@ sendButton.addEventListener('click', function () {
   const avatar = document.querySelector('.profile-2 img').src;
 
   chat.innerHTML += `<div class="chat profile-2"><img src="${avatar}"><p>${text}</p><span>${new Date()}</span></div>`;
-	document.getElementById('input-footer').value = '';
+  document.getElementById('input-footer').value = '';
   // window.scrollBottom();
 });
