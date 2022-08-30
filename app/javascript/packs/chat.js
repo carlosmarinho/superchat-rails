94fa5419ad41b266c05f5aa5380c2f16846bdac7
Carlos Marinho <carluizfla@hotmail.com>
2022-08-29 22:12:21 -0300

    create function Send to chat

--- app/javascript/packs/chat.js
+++ app/javascript/packs/chat.js
@@ -10,38 +10,42 @@ var chat = document.querySelector('.content-chat');
 var back = document.querySelector('.header-icons-2');
 
 btn.addEventListener('click', function () {
-  if (container.style.display === 'block') {
-    container.style.display = 'none';
-  } else {
-    container.style.display = 'block';
-  }
+    if (container.style.display === 'block') {
+        container.style.display = 'none';
+    } else {
+        container.style.display = 'block';
+    }
 });
 
 btn2.addEventListener('click', function () {
-  if (container2.style.display === 'block') {
-    container2.style.display = 'none';
-  } else {
-    container2.style.display = 'block';
-  }
+    if (container2.style.display === 'block') {
+        container2.style.display = 'none';
+    } else {
+        container2.style.display = 'block';
+    }
 });
 
 conversations.forEach((conversation) => {
-  conversation.addEventListener('click', function () {
-    // comentei aqui tenho q ver como vai ficar com essa seta ou não
-    // if (chat.style.display === 'block') {
-    //   chat.style.display = 'none';
-    // } else {
-    //   chat.style.display = 'block';
-    // }
+    conversation.addEventListener('click', function () {
+        // comentei aqui tenho q ver como vai ficar com essa seta ou não
+        // if (chat.style.display === 'block') {
+        //   chat.style.display = 'none';
+        // } else {
+        //   chat.style.display = 'block';
+        // }
 
-    window.location = `/?id=${conversation.dataset.conversationId}`;
-  });
+        window.location = `/?id=${conversation.dataset.conversationId}`;
+    });
 });
 
 back.addEventListener('click', function () {
-  if (chat.style.display === 'none') {
-    chat.style.display = 'block';
-  }
+    if (chat.style.display === 'none') {
+        chat.style.display = 'block';
+    }
 });
 
+function Send() {
+    var env = document.getElementById('input-footer').value;
 
+    console.log('o que é isso...', env);
+}
