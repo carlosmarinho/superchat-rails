e6e7234b0d5a965d149ac47688b9cb4470da528e
Carlos Marinho <carluizfla@hotmail.com>
2022-08-31 09:46:07 -0300

    fetch listener conversation

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,13 @@ class User < ApplicationRecord
               file_content_type: { allow: ['image/jpeg', 'image/png', 'image/gif'] }
   
   def self.get_conversations(user_id)
-    # puts "current_user: ", User.current_scope
-    Conversation.where("writer_id = ?", user_id)
+    conversations = Conversation.where("writer_id = ?  ", user_id)
+    listener_ids = conversations.pluck(:listener_id)
+    
+    conversations2 = Conversation.where("listener_id = ? and writer_id not in (?)", user_id, listener_ids)
+      .select("id, writer_id as listener_id, listener_id as writer_id, created_at, updated_at")
+
+    conversations + conversations2
   end
 
 end
